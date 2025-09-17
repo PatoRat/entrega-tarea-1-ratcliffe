08045c2a6047687503778bd617b0801767e08e0d
Patricio <ratcliffepatricio321@gmail.com>
2025-09-17 01:50:30 -0300

    Primera parte, sin styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { ListaProductos } from "@/components/listaproductos";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Galería como pestaña hecha</Text>
-    </View>
+    <SafeAreaView style={styles.pestaña}>
+      <ListaProductos />
+    </SafeAreaView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  pestaña: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
---
+++ components/listaproductos.tsx
@@ -0,0 +1,66 @@
+import {
+    FlatList,
+    Image,
+    Text,
+    View
+} from "react-native";
+
+const IMAGENES = [
+    {
+        id: '1',
+        imagen: require("@/assets/images/tostadora.png"),
+        titulo: 'Tostadora',
+        descripcion: 'Nueva de SmartLife',
+        precio: '$65000'
+    },
+    {
+        id: '2',
+        imagen: { uri: 'https://www.lg.com/cac/images/microondas/md07583967/gallery/d1.jpg' },
+        titulo: 'Microondas',
+        descripcion: 'Alta calidad LG',
+        precio: '$146950'
+    },
+    {
+        id: '3',
+        imagen: { uri: 'https://osterar.vtexassets.com/arquivos/ids/157691-800-auto?v=638772137804400000&width=800&height=auto&aspect=true' },
+        titulo: 'Licuadora',
+        descripcion: 'Oster Argentina',
+        precio: '$240000'
+    },
+];
+
+type ItemProps = {
+    imagen: any,
+    titulo: string,
+    descripcion: string,
+    precio: string
+};
+
+const Item = ({ imagen, titulo, descripcion, precio }: ItemProps) => (
+    <View>
+        <Image source={imagen} />
+        <Text>{titulo}</Text>
+        <Text>
+            {descripcion}
+            <Text>
+                {precio}
+            </Text>
+        </Text>
+    </View>
+);
+
+export const ListaProductos = () => {
+    return (
+        <FlatList
+            data={IMAGENES}
+            renderItem={({ item }) =>
+                <Item
+                    imagen={item.imagen}
+                    titulo={item.titulo}
+                    descripcion={item.descripcion}
+                    precio={item.precio}
+                />}
+            keyExtractor={item => item.id}
+        />
+    );
+};
