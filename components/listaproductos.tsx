dd4b437b14e391fd808c15081895ff59fd82b0a2
Patricio <ratcliffepatricio321@gmail.com>
2025-09-17 13:01:53 -0300

    Hasta el TextInput hecho, falta styling y Modal

--- components/listaproductos.tsx
+++ components/listaproductos.tsx
@@ -1,35 +1,54 @@
 import Item from "@/components/Item";
 import { IMAGENES } from "@/scripts/data-and-tipe";
+import { useState } from "react";
 import {
     FlatList,
+    StyleSheet,
     TextInput,
     View
 } from "react-native";
 
 const ListaProductos = () => {
-    const cambiarTexto = () => { }
+    const [busqueda, setBusqueda] = useState("");
+
     return (
         <>
             <TextInput
                 style={{ margin: 10 }}
-                onChangeText={cambiarTexto}
-                value={"text"}
+                onChangeText={setBusqueda}
+                value={busqueda}
             />
             <FlatList
                 data={IMAGENES}
-                renderItem={({ item }) =>
-                    <Item
-                        imagen={item.imagen}
-                        titulo={item.titulo}
-                        precio={item.precio}
-                    />}
+                renderItem={({ item }) => {
+                    if (item.titulo.toLocaleLowerCase().startsWith(busqueda.toLocaleLowerCase())) {
+                        return (
+                            <Item
+                                imagen={item.imagen}
+                                titulo={item.titulo}
+                                precio={item.precio}
+                            />
+                        )
+                    }
+                    else {
+                        return (
+                            <View style={styles.separador} />
+                        )
+                    }
+                }}
                 keyExtractor={item => item.id}
                 ItemSeparatorComponent={() => (
-                    <View style={{ margin: 10 }} />
+                    <View style={styles.separador} />
                 )}
             />
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    separador: {
+        margin: 10
+    },
+});
 
 export default ListaProductos;
